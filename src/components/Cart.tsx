2397441a590f6622ee738048472f0f1cd4cfb967
tanbiralam <itzmetanbir@gmail.com>
2024-03-09 15:21:38 +0530

    cart issue fix

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -13,9 +13,10 @@ import { Separator } from "./ui/separator";
 import { formatPrice } from "@/lib/utils";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
+import Image from "next/image";
 
 const Cart = () => {
-  const itemCount = 2;
+  const itemCount = 0;
   const fee = 200;
 
   return (
@@ -72,7 +73,29 @@ const Cart = () => {
             </div>
           </>
         ) : (
-          <div></div>
+          <div className="flex flex-col items-center justify-center space-y-1 h-full">
+            <div
+              aria-hidden="true"
+              className="relative mb-4 h-60 w-60 text-muted-foreground"
+            >
+              <Image src="/hippo-empty-cart.png" fill alt="empty-cart" />
+            </div>
+
+            <div className="text-xl font-semibold">Your cart is empty</div>
+
+            <SheetTrigger asChild>
+              <Link
+                href="/products"
+                className={buttonVariants({
+                  variant: "link",
+                  size: "sm",
+                  className: "text-sm text-muted-foreground",
+                })}
+              >
+                Add Items to your cart to Checkout
+              </Link>
+            </SheetTrigger>
+          </div>
         )}
       </SheetContent>
     </Sheet>
